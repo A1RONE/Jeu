0768c219af19072ef29ee14c53110ed4349717d9
erwann <erwann.perrin@etudiant.univ-rennes1.fr>
2023-03-18 15:10:55 +0100

    ajout d'une page generique

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+
+
+<head>
+    <meta charset="utf-8" />
+    <title>ListeJeu</title>
+    <style></style>
+
+    <link rel="stylesheet" type="text/css" href="ToDoGame.css">
+
+    <meta name="keywords" content="ToDo, Game">
+    <meta name="author" content="AIRONE" >
+
+</head>
+
+<body>
+
+</body>
---
+++ static/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<div>
+<p>Auteur: Erwann Perrin</p>
+<p>Contact</p>
+</div>
+
+<p>© 2023 CUPGE2</p>
+</footer>
